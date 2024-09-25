ccdb2fc4cd76cf4b378c876981e658c61488fc4e
q0d1r0v <qodirovoybekjon08@gmail.com>
2024-09-25 15:07:08 +0500

    softvision

---
+++ pages/[404].vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <div>
+      <img
+        src="../assets/not-found.svg"
+        alt="#not_found"
+        class="lg:w-[600px] w-[340px]"
+      />
+      <div class="flex items-center justify-center mt-14">
+        <el-button type="primary" plain size="large" @click="$router.push('/')"
+          >Bosh sahifaga qaytish</el-button
+        >
+      </div>
+    </div>
+  </div>
+</template>
